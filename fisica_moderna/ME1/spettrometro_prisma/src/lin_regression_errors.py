bd82dce0e6e8898b007d82f6c1dc51fe140664e0
Lorenzo Liuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2022-12-19 14:19:53 +0100

    prisma ok

--- fisica_moderna/ME1/spettrometro_prisma/src/lin_regression_errors.py
+++ fisica_moderna/ME1/spettrometro_prisma/src/lin_regression_errors.py
@@ -13,6 +13,8 @@ plt.title(names[3])
 plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0.0))
 
 ax.errorbar(x_data[0], y_data[0], xerr=x_data[1], yerr=y_data[1], fmt='.', color='blue')
+m, b = np.polyfit(x_data[0], y_data[0], 1)
+ax.plot(x_data[0], m * x_data[0] + b, ':r', label=f'$y = ({m:.1e})x + {b:.3f}$')
 
 ax.set_xlabel(names[4])
 ax.set_ylabel(names[5])
@@ -20,3 +22,6 @@ ax.legend(loc='upper left')
 
 plt.tight_layout()
 plt.savefig(names[2], dpi=250)
+
+# python src/lin_regression_errors.py data/1_lambdas2.dat data/n.dat images/Cauchy_2.png 'Regressione lineare' '$1 / \lambda^2 [m^{-2}]$' '$n(\lambda)$'
+# python src/lin_regression_errors.py data/1_lambdas2.dat data/n2.dat images/Cauchy_2.png 'Regressione lineare' '$1 / \lambda^2 [m^{-2}]$' '$n^2(\lambda)$'
